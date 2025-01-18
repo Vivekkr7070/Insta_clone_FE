e9b17a15e2a3abe4500221ecefb12e4bb74a7a9c
Vivek Kumar <vivekkr8541@gmail.com>
2025-01-18 21:05:52 +0530

    changes in localhost setting

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:6001', // Replace with your API endpoint
-  // baseURL: 'https://insta-clone-i8e1.onrender.com', // Replace with your API endpoint
+  // baseURL: 'http://localhost:6001', // Replace with your API endpoint
+  baseURL: 'https://insta-clone-i8e1.onrender.com', // Replace with your API endpoint
   headers: { 'Content-Type': 'application/json' },
 });
 
